1ce23a45726a147acdbf8a4b5e4480ed235bfc53
He Wang <wanghe020608@gmail.com>
2024-02-29 14:53:20 +0800

    adding wsap, we can see cmpgt now

--- human_wsap.py
+++ human_wsap.py
@@ -5,6 +5,8 @@
 # datatype: INT8
 # CIM is bit parallel
 
+# Mode - WSAP
+
 from hw_config import hwc
 from hw_config import Config
 import write_history 
@@ -13,11 +15,11 @@ import math as m
 import torch
 
 # 可变参数
-config = Config(al=256, pc=16, scr=16, is_depth=512, os_depth=1024)
+config = Config(al=128, pc=16, scr=16, is_depth=512, os_depth=1024)
 
 acc0 = hwc(config)
 
-gli = ['mvm', (55, 768, 64)]
+gli = ['mvm', (32, 2304, 64)]
 
 # 两个length对应作点乘，channel互相无关
 weight_map_channel = gli[1][0]
@@ -27,18 +29,21 @@ input_map_length  = gli[1][1]
 input_map_channel  = gli[1][2]
 
 # 计算input map到IS的mapping
-# region 对于Input_map 到 IS 的 mapping, 我们首先需要将一个channel的数据放入IS，如果一个channel放完，IS当前行未满，则后续补0，新的channel另起一行
-input_data_per_row = m.floor(acc0.InputSRAMWidth / config.DATA_WIDTH)
-rows_per_input_channel = m.ceil(input_map_length / input_data_per_row)
-input_channels_per_ISload = m.floor(acc0.InputSRAMDepth / rows_per_input_channel)
-IS_load_times_per_inst = m.ceil(input_map_channel / input_channels_per_ISload)
+# region 载入尽量多的input vector, min{lac*scr, lat},只存一次
+data_length_stored_IS = min(config.AL * config.SCR, input_map_length)
+input_data_per_row = acc0.InputSRAMWidth // config.DATA_WIDTH
+rows_per_input_channel = m.ceil(data_length_stored_IS / input_data_per_row) # 与isap不同
+input_channels_per_ISload = min(acc0.InputSRAMDepth // rows_per_input_channel, input_map_channel)
+
+IS_load_times_per_inst = input_map_channel // input_channels_per_ISload
 
 IS_load_rows = [input_channels_per_ISload * rows_per_input_channel] * (IS_load_times_per_inst)
 if input_map_channel % input_channels_per_ISload != 0:
     IS_load_rows[IS_load_times_per_inst-1] = input_map_channel % input_channels_per_ISload * rows_per_input_channel
+
 # endregion
 
-# 将weight map切成CIM size的block，放入CIM中
+# region 将weight map切成CIM size的block，放入CIM中
 weight_block_row = m.ceil(weight_map_channel / config.PC)
 weight_block_col = m.ceil(weight_map_length / config.AL)
 weight_block_num = weight_block_col * weight_block_row
@@ -47,6 +52,7 @@ weight_update_times_per_inst = m.ceil(weight_block_num / config.SCR)
 weight_update_ls = [config.SCR] * weight_update_times_per_inst
 if weight_block_num % config.SCR != 0:
     weight_update_ls[weight_update_times_per_inst-1] = weight_block_num % config.SCR
+# endregion
 
 para_times = weight_block_row
 acc_times = weight_block_col
@@ -59,7 +65,6 @@ for i_pt in range(para_times):
         ls_matrix[i_pt,i_at] = ls_fg
         ls_fg = ls_fg+1
         if ls_fg == config.SCR: ls_fg = 0
-# endregion
 
 # atos matrix 代表了这一次计算的外部数据流方向
     # 0:aor reg = reg + psum
@@ -79,18 +84,22 @@ for i_pt in range(para_times):
                 row_st_fg = 1
             else:
                 atos_matrix[i_pt,i_at] = aos
-
+# endregion
+                
 def LOG_INIT():
-    with open('mi.log','w') as f:
+    with open('wsap.log','w') as f:
         f.write("starting compiler:\n")
 
 def IDLE():
-    with open('mi.log','a') as f:
+    with open('wsap.log','a') as f:
         f.write("nop\n")
 
 def LOADIS_BLOCK(num_rows, input_map_position): #输入现在正要存的数据在input map中的位置，以及需要输入多少行
-    with open('mi.log','a') as f:
+    with open('wsap.log','a') as f:
         for i_rows in range(num_rows):
+            # input_map_position = (i_rows // rows_per_input_channel) * input_map_length # channel 对应的
+            # + (i_rows % rows_per_input_channel) * acc0.InputSRAMWidth // config.DATA_WIDTH # row 对应的
+            # + int(config.BUS_WIDTH / config.DATA_WIDTH) * (acc0.InputSRAMWidth//acc0.BusWidth - 1) #一行内用于递减的position reg
             input_map_position += int(config.BUS_WIDTH / config.DATA_WIDTH) * (acc0.InputSRAMWidth//acc0.BusWidth - 1)
             for j_reg in reversed(range(acc0.InputSRAMWidth//acc0.BusWidth)):
                 if j_reg != 0:
@@ -98,12 +107,13 @@ def LOADIS_BLOCK(num_rows, input_map_position): #输入现在正要存的数据�
                 else:
                     f.write("lis\t\t"+str(j_reg)+"\t"+str(i_rows)+"\t"+str(input_map_position)+"\n")
                 input_map_position -= int(config.BUS_WIDTH / config.DATA_WIDTH)
-                # !!! 每一个channel的最后一行，可能要填0
-            input_map_position += int(config.BUS_WIDTH / config.DATA_WIDTH) * (acc0.InputSRAMWidth//acc0.BusWidth)
+            input_map_position += acc0.InputSRAMWidth // config.DATA_WIDTH + config.BUS_WIDTH // config.DATA_WIDTH #补偿最后一次多减了一个
+            if i_rows % rows_per_input_channel == rows_per_input_channel - 1:
+                input_map_position += input_map_length - rows_per_input_channel * acc0.InputSRAMWidth // config.DATA_WIDTH 
     return input_map_position
-
-def WU_LSBANK(num_ls, num_channel, i_block): #输入要存几个channel，几个local switch，从第几个block开始，注：在isap下block按行输入，
-    with open('mi.log','a') as f:
+        
+def WU_LSBANK(num_ls, num_channel, i_block): #输入要存几个channel，几个local switch，从第几个block开始，注：在wsap下block按行输入，
+    with open('wsap.log','a') as f:
         for i_ls in range(num_ls):
             i_pt = i_block // weight_block_col
             i_at = i_block % weight_block_col
@@ -122,7 +132,7 @@ def WU_LSBANK(num_ls, num_channel, i_block): #输入要存几个channel，几个
                                 "\t" + str(weight_map_position)+"\n")
             i_block += 1
 
-def CMPFIS_INT8(i_input_channel, computing_block):# 输入channel, computing block, 对当前channel内所有内容遍历CIM进行计算
+def COMPUTE(i_input_channel, computing_block):# 输入channel, computing block, 对当前channel内所有内容遍历CIM进行计算
     # bit-parallel
     i_ls = computing_block % config.SCR
     i_pt = computing_block // weight_block_col
@@ -145,69 +155,73 @@ def CMPFIS_INT8(i_input_channel, computing_block):# 输入channel, computing blo
     
     if n != 0:
         read_command = "read_OS_line "+str(os_addr)
-        add_read_n_lines_before('mi.log', n, read_command)
+        add_read_n_lines_before('wsap.log', n, read_command)
 
-    with open('mi.log','a') as f:
-        f.write("cmpfis_int8\t" + str(is_addr) + '\t' + str(i_ls) + '\t' + str(atos_flag) + '\t' +str(os_addr) + '\n')
-                # + '\t' + "ws = " + str(write_status) + '\t' + "n = " + str(n) + '\n')
+    with open('wsap.log','a') as f:
+        if i_at <= rows_per_input_channel-1:
+            f.write("cmpfis\t" + str(is_addr) + '\t' + str(i_ls) + '\t' + str(atos_flag) + '\t' +str(os_addr) + '\n')
+                    # + '\t' + "ws = " + str(write_status) + '\t' + "n = " + str(n) + '\n')
+        else:
+            input_map_position = i_input_channel * input_map_length + i_at * config.AL + int(config.BUS_WIDTH / config.DATA_WIDTH) * (acc0.InputSRAMWidth//acc0.BusWidth - 1)
+            for j_reg in reversed(range(acc0.InputSRAMWidth//acc0.BusWidth)):
+                if j_reg != 0:
+                    f.write("cmpgtp\t" + str(i_ls) + '\t' + str(atos_flag) + '\t' +str(os_addr)
+                            +str(j_reg)+str(input_map_position)+"\n")
+                else:
+                    f.write("cmpgt\t" + str(i_ls) + '\t' + str(atos_flag) + '\t' +str(os_addr)
+                            +str(j_reg)+str(input_map_position)+"\n")
+                input_map_position -= int(config.BUS_WIDTH / config.DATA_WIDTH)
     ws_history.update("write_status") 
-        
+
 
 LOG_INIT()
-input_map_position = 0
+i_block = 0
 ws_history = write_history.WriteStatusHistory()
-for i_IS_load in range(IS_load_times_per_inst):
-    input_map_position = LOADIS_BLOCK(num_rows = IS_load_rows[i_IS_load], 
-                                      input_map_position = input_map_position)
-
-    i_block = 0
-
-    for j_weight_load in range(weight_update_times_per_inst):
-        WU_LSBANK(num_ls = weight_update_ls[j_weight_load], 
-                  num_channel = config.PC,  # !!!可能有很多channel浪费
-                  i_block = i_block)
-        
-        for i in range (acc0.CIMsWriteWidth//acc0.BusWidth*config.WEIGHT_ROW):
-            IDLE()
-
-        for i_input_channel in range(i_IS_load * input_channels_per_ISload, 
-                                     i_IS_load * input_channels_per_ISload + 
-                                     IS_load_rows[i_IS_load] // rows_per_input_channel): # 选中一个input channel
-            for j_ls in range(weight_update_ls[j_weight_load]): #选中一个ls
+for i_weight_update in range(weight_update_times_per_inst):
+    WU_LSBANK(num_ls = weight_update_ls[i_weight_update], 
+                num_channel = config.PC,  # !!!可能有很多channel浪费
+                i_block = i_block)
+    for i in range (acc0.CIMsWriteWidth//acc0.BusWidth*config.WEIGHT_ROW):
+        IDLE()
+    input_map_position = 0
+    for j_IS_load in range(IS_load_times_per_inst):
+        input_map_position = LOADIS_BLOCK(num_rows = IS_load_rows[j_IS_load], 
+                                            input_map_position = input_map_position)
+        for i_input_channel in range(j_IS_load * input_channels_per_ISload, 
+                                        j_IS_load * input_channels_per_ISload + 
+                                        IS_load_rows[j_IS_load] // rows_per_input_channel):
+            for j_ls in range(weight_update_ls[i_weight_update]): #选中一个ls
                 j_compute_block = i_block + j_ls
-                CMPFIS_INT8(i_input_channel = i_input_channel, 
+                COMPUTE(i_input_channel = i_input_channel, 
                             computing_block = j_compute_block)
-
-        i_block += weight_update_ls[j_weight_load]
-
-
-
+    i_block += weight_update_ls[i_weight_update]
 
 # !!! 以下为测试用输出，勿删
 
-# print("System config:")
-# for attr, value in config.__dict__.items():
-#     print(f"{attr} = {value}")
+print("System config:")
+for attr, value in config.__dict__.items():
+    print(f"{attr} = {value}")
 
-# print("\nHardware config:")
-# for attr, value in acc0.__dict__.items():
-#     print(f"{attr} = {value}")
+print("\nHardware config:")
+for attr, value in acc0.__dict__.items():
+    print(f"{attr} = {value}")
 
-# print("\nInput map mapping:")
-# print(f"input_data_per_row = {input_data_per_row}")
-# print(f"rows_per_input_channel = {rows_per_input_channel}")
-# print(f"input_channels_per_ISload = {input_channels_per_ISload}")
-# print(f"IS_load_times_per_inst = {IS_load_times_per_inst}")
+print("\nInput map mapping:")
+print(f"data_length_stored_IS = {data_length_stored_IS}")
+print(f"input_data_per_row = {input_data_per_row}")
+print(f"rows_per_input_channel = {rows_per_input_channel}")
+print(f"input_channels_per_ISload = {input_channels_per_ISload}")
+print(f"IS_load_times_per_inst = {IS_load_times_per_inst}")
     
-# print(f"IS_load_rows = {IS_load_rows}")
+print(f"IS_load_rows = {IS_load_rows}")
 
-# print("\nWeight map mapping:")
-# print(f"weight_block_row = {weight_block_row}")
-# print(f"weight_block_col = {weight_block_col}")
-# print(f"weight_block_num = {weight_block_num}")
-# print(f"weight_update_times_per_inst = {weight_update_times_per_inst}")
+print("\nWeight map mapping:")
+print(f"weight_block_row = {weight_block_row}")
+print(f"weight_block_col = {weight_block_col}")
+print(f"weight_block_num = {weight_block_num}")
+print(f"weight_update_times_per_inst = {weight_update_times_per_inst}")
         
-# print(f"weight_update_ls = {weight_update_ls}")
+print(f"weight_update_ls = {weight_update_ls}")
         
-# print("ls_matrix:\n", ls_matrix, "\n")
-# print("atos_matrix:\n", atos_matrix, "\n")
+print("ls_matrix:\n", ls_matrix, "\n")
+print("atos_matrix:\n", atos_matrix, "\n")
